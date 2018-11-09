0020f33cdb2d8b2cd2f73b6c8e7c773968cc1647
Bhairav Mehta <bhairavmehta95@gmail.com>
2018-11-08 22:22:48 -0500

    Fixing bug where policy does'nt hit max timesteps
    thanks orlando!

--- duckietown_rl/utils.py
+++ duckietown_rl/utils.py
@@ -57,15 +57,17 @@ class ReplayBuffer(object):
         }
 
 
-def evaluate_policy(env, policy, eval_episodes=10):
+def evaluate_policy(env, policy, eval_episodes=10, max_timesteps=500):
     avg_reward = 0.
     for _ in range(eval_episodes):
         obs = env.reset()
         done = False
-        while not done:
+        step = 0
+        while not done and step < max_timesteps:
             action = policy.predict(np.array(obs))
             obs, reward, done, _ = env.step(action)
             avg_reward += reward
+            step += 1
 
     avg_reward /= eval_episodes
 
